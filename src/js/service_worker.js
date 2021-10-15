2f4167102d4d7b4b87d7a5faa1e1c133487ce688
Rijan Timsina <regiontimsina@gmail.com>
2021-10-15 11:32:03 -0400

    started blacklist implementation

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -1,3 +1,20 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Service Worker installed');
+// a list of blacklisted urls
+const blacklistedUrls = [
+"facebook.com",
+"youtube.com",
+];
+
+chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        if (changeInfo.status == "complete") {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                var url = tabs[0].url;
+                for (var i = 0; i < blacklistedUrls.length; i++) {
+                    if (url.includes(blacklistedUrls[i])) {
+                        console.log("blacklisted url");
+                    }
+                }
+            });
+        }
+    });
 });
