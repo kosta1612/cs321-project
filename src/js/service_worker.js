63ed630779a74e5bac451089d3d0e397326d8643
rijtim1 <regiontimsina@gmail.com>
2021-10-16 10:02:00 -0400

    blacklisting feature

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -2,19 +2,23 @@
 const blacklistedUrls = [
 "facebook.com",
 "youtube.com",
+"mymasonportal.gmu.edu", // added for testing
+"patriotweb.gmu.edu"
 ];
 
-chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if (changeInfo.status == "complete") {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                var url = tabs[0].url;
-                for (var i = 0; i < blacklistedUrls.length; i++) {
-                    if (url.includes(blacklistedUrls[i])) {
-                        console.log("blacklisted url");
-                    }
-                }
-            });
+function alertUser(){
+    alert("You cannot visit this website");
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == "complete"){
+        console.log("Url change completed");
+        console.log("URL Loaded: " + tab.url);
+        for (var i = 0; i < blacklistedUrls.length; i++) {
+            if (tab.url.includes(blacklistedUrls[i])) {
+                console.log("User visited a blacklisted url");
+                alertUser();
+            }
         }
-    });
+    }
 });
