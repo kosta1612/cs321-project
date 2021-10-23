6134bfeb14bf9e25b417938eb853577cc6992ae2
rijtim1 <regiontimsina@gmail.com>
2021-10-23 18:12:58 -0400

    added more blacklisted pages

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -1,24 +1,28 @@
 // a list of blacklisted urls
+
 const blacklistedUrls = [
 "facebook.com",
 "youtube.com",
-"mymasonportal.gmu.edu", // added for testing
-"patriotweb.gmu.edu"
+"mymasonportal.gmu.edu",
+"patriotweb.gmu.edu",
+"reddit.com",
 ];
 
-function alertUser(){
-    alert("You cannot visit this website");
+function alertUser(msgs){
+    alert(msgs);
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == "complete"){
-        console.log("Url change completed");
-        console.log("URL Loaded: " + tab.url);
+        // console.log("Url change completed");
+        // console.log("URL Loaded: " + tab.url);
         for (var i = 0; i < blacklistedUrls.length; i++) {
             if (tab.url.includes(blacklistedUrls[i])) {
-                console.log("User visited a blacklisted url");
-                alertUser();
+                // console.log("User visited a blacklisted url");
+                alertUser("You are not allowed to visit this website");
+                let url = chrome.runtime.getURL("blacklisted.html");
             }
         }
     }
 });
+
